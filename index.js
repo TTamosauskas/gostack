e3787f0f00a60dad3b4503f6f25bb0c24c63e5b5
TTamosauskas <thiago.gt@gmail.com>
2020-03-10 16:55:45 -0300

    demais rotas do CRUD e middlewares

--- index.js
+++ index.js
@@ -4,16 +4,45 @@ const server = express();
 
 server.use(express.json());
 
+// Variaveis
+
 const projects = [
   { id: '1', title: 'Novo projeto', tasks: [] }
 ];
 
+let requisicoes = 0;
 
-server.get('/projects', (req, res) => {
+let project = null;
+
+// Midlewares
+
+function contagem(req, res, next) {
+  console.log("Requisição " + requisicoes);
+  requisicoes = requisicoes + 1;
+  return next();
+ }
+
+function verificaId(req, res, next) {
+
+ const {index} = req.params;
+ 
+ project = projects.find(item => item.id === index);
+
+  if (!project){
+    return res.status(400).json({error:"ID Inexistente"});
+  }
+
+return next();
+}
+
+// Rotas
+
+
+server.get('/projects', contagem, (req, res) => {
   return res.json(projects)
 })
 
-server.post('/projects', (req,res) => {
+server.post('/projects', contagem, (req,res) => {
   
   const {id, title, tasks} = req.body;
 
@@ -22,22 +51,17 @@ server.post('/projects', (req,res) => {
   return res.json(projects)
 })
 
-server.put('/projects/:index', (req,res) => {
+server.put('/projects/:index', contagem, verificaId, (req,res) => {
 
-  const {index} = req.params;
   const {title} = req.body;
-  const project = projects.find(item => item.id === index);
-  
+   
   project.title = title;
 
   return res.json(projects)
 })
 
 
-server.delete('/projects/:index', (req,res) => {
-
-  const {index} = req.params;
-  const project = projects.find(item => item.id === index);
+server.delete('/projects/:index', contagem, verificaId, (req,res) => {
 
   projects.splice(project,1)
 
@@ -45,18 +69,14 @@ server.delete('/projects/:index', (req,res) => {
 })
 
 
-server.post('/projects/:index/tasks', (req,res) => {
+server.post('/projects/:index/tasks', contagem, verificaId, (req,res) => {
    
   const {title} = req.body;
-
-  const {index} = req.params;
-  const project = projects.find(item => item.id === index);
 
   project.tasks.push(title);
 
   return res.json(projects)
 })
-
 
 
 server.listen(3000)
